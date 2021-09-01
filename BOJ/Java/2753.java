e862ca65b171e7e3b03725a59f5f71b5893a533e
박준수 <tony180322@gmail.com>
2021-09-01 17:54:02 +0900

    solve: BOJ 2753 in Java

---
+++ BOJ/Java/2753.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int year = input.nextInt();
+
+        if(year % 4 == 0 && year % 100 != 0) {
+            System.out.print(1);
+        }
+        else if(year % 4 == 0 && year % 400 == 0) {
+            System.out.print(1);
+        }
+        else {
+            System.out.print(0);
+        }
+    }
+}
