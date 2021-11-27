e2e6dc40408665dee60c1e9e9043367e744a0b61
박준수 <tony180322@gmail.com>
2021-11-27 23:23:32 +0900

    solve: BOJ 10991 in Python

---
+++ BOJ/Python/10991.py
@@ -0,0 +1,9 @@
+num = int(input())
+
+for i in range(num):
+    for j in range(num - i - 1):
+        print(' ', end = '')
+    print('*', end = '')
+    for j in range(i):
+        print(' *', end = '')
+    print('')
