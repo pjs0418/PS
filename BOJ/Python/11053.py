5f77191dd9342029ba56daf924f9317335e91d85
박준수 <tony180322@gmail.com>
2021-11-28 16:15:04 +0900

    solve: BOJ 11053 in Python

---
+++ BOJ/Python/11053.py
@@ -0,0 +1,18 @@
+num = int(input())
+dp = []
+t = 0
+
+temp = input().split()
+list_int = list(map(int, temp))
+
+dp.append(1)
+
+for i in range(1, num):
+    for j in range(i):
+        if list_int[j] < list_int[i]:
+            if t < dp[j]:
+                t = dp[j]
+    dp.append(t + 1)
+    t = 0
+
+print(max(dp))
