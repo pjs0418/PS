26f2f743a0e502667be9fc16919df0b3885740e8
박준수 <tony180322@gmail.com>
2022-02-08 17:42:18 +0900

    solve: BOJ 2609 in Java

---
+++ BOJ/Java/2609.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int firstNum = Integer.parseInt(st.nextToken());
+        int secondNum = Integer.parseInt(st.nextToken());
+        int minResult = 1;
+
+        for(int i = 2;i < Math.min(firstNum, secondNum) + 1;i++) {
+            if(firstNum % i == 0 && secondNum % i == 0)
+                minResult = i;
+        }
+
+        System.out.println(minResult);
+        System.out.println(minResult * (firstNum / minResult) * (secondNum / minResult));
+
+        br.close();
+    }
+}
