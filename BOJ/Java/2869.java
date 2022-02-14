56e4098003d2e249fc2839584c6fe7566a3bc26f
박준수 <tony180322@gmail.com>
2022-02-14 18:02:07 +0900

    solve: BOJ 2869 in Java

---
+++ BOJ/Java/2869.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int num = (int)Math.ceil((V - A) / (double)(A - B));
+
+        System.out.println(++num);
+
+        br.close();
+    }
+}
