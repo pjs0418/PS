0644f990e7d1b9f3ddbcb2a21681245492030788
박준수 <tony180322@gmail.com>
2022-02-14 17:46:58 +0900

    solve: BOJ 7568 in Java

---
+++ BOJ/Java/7568.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for(int i = 0;i < N;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0;i < N;i++) {
+            int count = 0;
+
+            for(int j = 0;j < N;j++) {
+                if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) count++;
+            }
+
+            count++;
+            sb.append(count + " ");
+        }
+
+        System.out.println(sb);
+
+        br.close();
+    }
+}
