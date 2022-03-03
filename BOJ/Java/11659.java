adb653824fa0822fecfc2b6e66071bf748077599
박준수 <tony180322@gmail.com>
2022-03-03 23:05:54 +0900

    solve: BOJ 11659 in Java

---
+++ BOJ/Java/11659.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < N + 1; i++) arr[i] = Integer.parseInt(st.nextToken()) + arr[i - 1];
+
+        for (int a = 0; a < M; a++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            System.out.println(arr[j] - arr[i - 1]);
+        }
+
+        br.close();
+    }
+}
