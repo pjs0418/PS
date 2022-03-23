5fdf7a487bdc4638534a08b2de32fd9ad5ec156b
박준수 <tony180322@gmail.com>
2022-03-23 19:41:56 +0900

    solve: BOJ 1927 in Java

---
+++ BOJ/Java/1927.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+
+            if (input == 0) {
+                if (queue.isEmpty()) System.out.println(0);
+                else System.out.println(queue.poll());
+            } else queue.add(input);
+        }
+
+        br.close();
+    }
+}
