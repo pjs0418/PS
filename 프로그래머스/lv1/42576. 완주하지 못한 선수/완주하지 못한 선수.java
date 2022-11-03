da5cf5cfb70dc7e6c193c9ccaa3d757934133cce
pjs0418 <33286894+pjs0418@users.noreply.github.com>
2022-11-03 11:30:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.50 ms, Memory: 95.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0;i < participant.length;i++)
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        
+        for(int i = 0;i < completion.length;i++)
+            map.put(completion[i], map.getOrDefault(completion[i], 0) - 1);
+        
+        for(Map.Entry<String,Integer> entrySet : map.entrySet()) {
+            if(entrySet.getValue() == 1)
+                answer = entrySet.getKey();
+        }
+        
+        return answer;
+    }
+}
